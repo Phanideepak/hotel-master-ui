c4f322d57930670048c782e3203b081da5e931bf
Leon Vladimar <phanideepak1729@gmail.com>
2023-02-25 13:36:52 +0530

    hotel-booking

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,40 @@
+import { faBed, faCar, faPlane, faTaxi } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 import './header.css'
 
 const Header = () => {
   return (
     <div className='header'>
-        <div className="headerList">
-            <div className="headerListItems">
-                
+        <div className="headerContainer">
+            <div className="headerList">
+                <div className="headerListItem">
+                <FontAwesomeIcon icon={faBed} />
+                <span>Stays</span>
+                </div>
+                <div className="headerListItem active">
+                <FontAwesomeIcon icon={faPlane} />
+                <span>Flights</span>
+                </div>
+                <div className="headerListItem">
+                <FontAwesomeIcon icon={faCar} />
+                <span>Car Rentals</span>
+                </div>
+                <div className="headerListItem">
+                <FontAwesomeIcon icon={faBed} />
+                <span>Attractions</span>
+                </div>
+                <div className="headerListItem">
+                <FontAwesomeIcon icon={faTaxi} />
+                <span>Airport Taxis</span>
+                </div>
             </div>
+            <h1 className="headerTitle">A lifetime of discounts for anime lovers.</h1>
+            <p className="headerDescription">
+                Get rewarded for travels. Unlock instant savings of 10% or more with 
+                account.
+            </p>
+            <button className="headerButton">Sign in / Register</button>
         </div>
     </div>
   )
